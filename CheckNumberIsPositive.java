bc3a4cc170e54e7b5ee593d201fece2eb768f15d
Gayathri2407 <ggayusairam@gmail.com>
2025-01-08 13:35:26 +0530

    Add files via upload

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class CheckNumberIsPositive {
+
+	static int i = 10;
+	static int j = -10;
+	
+	public static void main(String[] args) {
+		if(i>=0) {
+			System.out.println(i+" is a Positive Integer");
+		}
+		if(j<0){
+			System.out.println(j+ " is a Negative Integer");
+		}
+		
+	}
+
+	
+}
---
+++ IsPrimeNumber.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class IsPrimeNumber {
+
+	public static void main(String[] args) {
+
+		int i = 13;
+		if ((i % 2 != 0))
+		{
+			System.out.println(i + " is a Prime Number");
+		} else {
+			System.out.println(i + " is not a Prime Number");
+		}
+	}
+
+}
